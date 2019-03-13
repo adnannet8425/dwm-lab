c881ee041cb2f513b0229fbc0b29527d9bb2c910
Mohd Adnan Ansari <31682322+adnannet8425@users.noreply.github.com>
2019-03-13 17:08:00 +0530

    Updated With Out Bound

--- PageRank/page_rank.py
+++ PageRank/page_rank.py
@@ -1,14 +1,26 @@
 import numpy as np
+N=int(input("Enter The No. Of Pages => "))
+print("Enter 1 Page i having a link to Page j......")
+links=[]
+d=0.85
+for i in range(0,N):
+	L=[]
+	for j in range(0,N):
+		L.append(int(input("Page "+str(i+1)+" To Page "+str(j+1)+" => ")))
+		links.append(L)
 
-#Take matrix size as input
-n=int(input("Enter Total No, Pages ->"))
-print("Enter 1 If Page i Has Link To Page j")
-#initialise nxn matrix 
-mat=np.zeros((n,n))
+outboundL = np.zeros((N,),dtype=int)
+for i in range(0,N):
+	for j in range(0,N):
+		if(links[i][j]==1):
+			outboundL[i]+=1
 
-#input each row at a time,with each element separated by a space
-for i in range(n):
-   mat[i]=input("For Page "+str(i)+" To Others => ").split(" ")
-print(mat)  
+M=np.zeros((N,N))
+for i in range(0,N):
+	for j in range(0,N):
+		if(links[j][i]==1):
+			M[i][j]=1/outboundL[j]
 
-#outbound Calucalation
+col=np.ones((N,1))
+
+r=np.full((N,1),1/N)
